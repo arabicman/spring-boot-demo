6ddd4a55cd3cda97cca3287f28520a07903aa468
arabicman <8345807@qq.com>
2021-03-24 13:30:01 -0700

    3.24

---
+++ demo-sb02-hello/src/test/java/com/demo/demosb02hello/DemoSb02HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demosb02hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSb02HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
