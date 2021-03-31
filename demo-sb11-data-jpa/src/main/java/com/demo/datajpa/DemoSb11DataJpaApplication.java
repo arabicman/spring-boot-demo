4c02c74834431b944ae2bbd13aaf534ff794035f
arabicman <8345807@qq.com>
2021-03-30 22:30:16 -0700

    3.30

---
+++ demo-sb11-data-jpa/src/main/java/com/demo/datajpa/DemoSb11DataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.demo.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSb11DataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSb11DataJpaApplication.class, args);
+    }
+
+}
---
+++ demo-sb11-data-jpa/src/test/java/com/demo/datajpa/DemoSb11DataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSb11DataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
